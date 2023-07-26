bafa6212e0e2d96f4658de0fe73db3d45cfc2520
Daniel Agg <daniel.agg@outlook.com>
2023-07-26 20:58:33 +0200

    demo app warning

--- app/(login)/login/page.tsx
+++ app/(login)/login/page.tsx
@@ -34,7 +34,28 @@ export default function Login() {
           without deploying new code.
         </h2>
       </div>
-      <div className="w-[90%] lg:w-[43%] min-h-screen flex flex-col items-center justify-center">
+      <div className="w-[90%] lg:w-[43%] min-h-screen flex flex-col items-center justify-center relative">
+        <div className="absolute top-0 w-full bg-red-600 p-4 text-white flex items-center justify-center text-sm space-x-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+            />
+          </svg>
+
+          <div>
+            This is a demo application. Please feel free to press 'Login'
+            without any email/password specified.
+          </div>
+        </div>
         <h1 className="text-4xl font-bold">Welcome back!</h1>
         <p className="mt-1 opacity-70">Please sign in, to continue.</p>
 
